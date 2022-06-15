c1b2f6510a240783acc8c2d6eac3af125cb5051c
ermaccer <40604575+ermaccer@users.noreply.github.com>
2022-06-15 21:59:37 +0200

    clean up

--- GUNHook/dllmain.cpp
+++ GUNHook/dllmain.cpp
@@ -11,12 +11,6 @@ using namespace Memory::VP;
 
 void Init()
 {
-    AllocConsole();
-    freopen("CONIN$", "r", stdin);
-    freopen("CONOUT$", "w", stdout);
-    freopen("CONOUT$", "w", stderr);
-
-
     Patch(_pat("6A 00 68 ? ? ? ? 68 A5 A7 F5 FD", 3), Hooks::Camera_Constructor_Hook);
     Patch(*(int*)_pat("A1 ? ? ? ? 99 2B C2 D1 F8 50 FF 15", 13), Hooks::SetCursorPos_Hook);
     InjectHook(_pat("FF 15 ? ? ? ? 6A 20 E8 ? ? ? ? 3B C7", 8), eDirectX9Hook::Direct3DCreate9_Hook, PATCH_CALL);
